a75701b1b1041451eb6e7e475a9adfd73fcf79a2
Hui Wang <huiwang_m@outlook.com>
2025-07-28 10:16:15 +0200

    Create LoopVTU.py
    paraview draw vorticity images using .htg files, loop all the htg

---
+++ para_vorticity/LoopVTU.py
@@ -0,0 +1,21 @@
+# We loop all the vtu file using the paepared pvpython script
+from paraview.simple import *
+import numpy as np
+import face_vorticity as im
+import os
+
+#get the filename list from a certain directory
+filelist = []
+for t in np.arange(0., 0.0901, 0.00072):
+  file = "paraHTG_%06g.htg" % (t)
+  filelist.append(file)
+print(filelist)
+
+path = "/media/huier/Mybook_12T/Axi_shape/2htg/Dh38/Re2500/We800/"
+print(path)
+
+#loop every htg file from the list
+for item in filelist:
+  print("Processing"+item)
+  im.Bloop(path,item)
+  ResetSession()
